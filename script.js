2223862c6c22e83b03173b5e2a4cf12fe6672e9e
Hayri Unal <114813642+hayriunal@users.noreply.github.com>
2023-01-31 19:36:01 +0300

    it s Almost Finish .

--- script.js
+++ script.js
@@ -1,111 +1,99 @@
-const display = document.querySelector('.calculator-input');
-const keys = document.querySelector('.calculator-keys');
+const display = document.querySelector(".calculator-input");
+const keys = document.querySelector(".calculator-keys");
 
-let displayValue = '0';
+let displayValue = "0";
 let firstValue = null;
 let operator = null;
-let waitingForSecondValue = false ;
-
+let waitingForSecondValue = false;
 
 updateDisplay();
-
 
 function updateDisplay() {
-    display.value = displayValue;
+  display.value = displayValue;
 }
 
-keys.addEventListener('click',function(e){
+keys.addEventListener("click", function (e) {
   const element = e.target;
+
+  if (!element.matches("button")) return;
+
   
 
-if(!element.matches('button'))return;
-console.log(element);
+  if (element.classList.contains("operator")) {
+    // console.log('operator',element.value);
+    handleOperator(element.value);
+    updateDisplay();
 
-if(element.classList.contains('operator')){
- // console.log('operator',element.value);
- handleOperator(element.value);
- updateDisplay();
- 
+    return;
+  }
 
-  return;
-}
+  if (element.classList.contains("decimal")) {
+    //console.log('decimal',element.value);
+    inputDecimal();
+    updateDisplay();
+    return;
+  }
 
-if(element.classList.contains('decimal')){
-  //console.log('decimal',element.value);
-  inputDecimal(element.value);
+  if (element.classList.contains("clear")) {
+    //console.log('clear',element.value);
+    clear();
+    updateDisplay();
+    return;
+  }
+  //console.log('number',element.value);
+  inputNumber(element.value);
   updateDisplay();
-  return;
-}
-
-if(element.classList.contains('clear')){
-  //console.log('clear',element.value);
-  clear();
-  updateDisplay();
-  return;
-}
-//console.log('number',element.value);
-inputNumber(element.value);
-updateDisplay();
-
 });
 
 function handleOperator(nextOperator) {
-   const value =parseFloat(displayValue);
-   if (firstValue ===null) {
+  const value = parseFloat(displayValue);
+
+  if (firstValue === null) {
     firstValue = value;
-   } else if (operator){
+  } else if (operator) {
     const result = calculate(firstValue, value, operator);
 
-    displayValue = String(result);
-    firstValue = result ;
-   }
+    displayValue = `${parseFloat(result.toFixed(7))}`;
+    firstValue = result;
+  }
 
+  //  if(operator&& waitingForSecondValue) {
+  //    operator = nextOperator;
+  //    return;
+  //  }
 
   waitingForSecondValue = true;
-  operator= nextOperator; 
-  console.log(displayValue,firstValue, operator, waitingForSecondValue);
-
-
+  operator = nextOperator;
+  console.log(displayValue, firstValue, operator, waitingForSecondValue);
 }
-function calculate(first, second, operator){
-  if (operator === '+'){
+function calculate(first, second, operator) {
+  if (operator === "+") {
     return first + second;
-  }else if (operator === '-'){
+  } else if (operator === "-") {
     return first - second;
-  }else if(operator === '*'){
-    return first *second;
-  }else if (operator==='/'){
-    return first /second;
+  } else if (operator === "*") {
+    return first * second;
+  } else if (operator === "/") {
+    return first / second;
   }
- return second
-
+  return second;
 }
 
-function inputNumber(num){
-  if(waitingForSecondValue) {
+function inputNumber(num) {
+  if (waitingForSecondValue) {
     displayValue = num;
     waitingForSecondValue = false;
+  } else {
+    displayValue = displayValue === "0" ? num : displayValue + num;
   }
-  else {
-  displayValue = displayValue === '0'? num: displayValue + num;
-  }
-console.log(displayValue,firstValue, operator, waitingForSecondValue);
+  console.log(displayValue, firstValue, operator, waitingForSecondValue);
 }
 
-function inputDecimal(){
-  if(!displayValue.includes('.')){
-    displayValue += '.';
+function inputDecimal() {
+  if (!displayValue.includes(".")) {
+    displayValue += ".";
   }
-
 }
-function clear (){
-  displayValue ='0';
+function clear() {
+  displayValue = "0";
 }
-
-
-
-
-
-
-
-
