0996f5ec86375aa1e3b6a26c2be923836d572c34
Hayri Unal <114813642+hayriunal@users.noreply.github.com>
2023-01-31 12:56:44 +0300

    I Add Something.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const display = document.querySelector('.calculator-input');
-const keys = document.querySelector('calculator-keys');
+const keys = document.querySelector('.calculator-keys');
 
 let displayValue = '0';
 
@@ -13,11 +13,48 @@ function updateDisplay() {
 
 keys.addEventListener('click',function(e){
   const element = e.target;
-  console.log(element);
+  
 
+if(!element.matches('button'))return;
+console.log(element);
+
+if(element.classList.contains('operator')){
+  console.log('operator',element.value);
+  return;
+}
+
+if(element.classList.contains('decimal')){
+  //console.log('decimal',element.value);
+  inputDecimal(element.value);
+  updateDisplay();
+  return;
+}
+
+if(element.classList.contains('clear')){
+  //console.log('clear',element.value);
+  clear();
+  updateDisplay();
+  return;
+}
+//console.log('number',element.value);
+inputNumber(element.value);
+updateDisplay();
 
 });
 
+function inputNumber(num){
+  displayValue = displayValue === '0'? num: displayValue + num;
+}
+
+function inputDecimal(){
+  if(!displayValue.includes('.')){
+    displayValue += '.';
+  }
+
+}
+function clear (){
+  displayValue ='0';
+}
 
 
 
